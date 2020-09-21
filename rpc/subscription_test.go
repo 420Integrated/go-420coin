583c8e4cfb11bd46189c576b4a7901051386e2e6
abvhiael stuart <abvhiael@420integrated.com>
2020-09-21 20:25:21 -0300

    Update subscription_test.go

--- rpc/subscription_test.go
+++ rpc/subscription_test.go
@@ -48,7 +48,7 @@ func TestNewID(t *testing.T) {
 
 func TestSubscriptions(t *testing.T) {
 	var (
-		namespaces        = []string{"eth", "shh", "bzz"}
+		namespaces        = []string{"420", "shh", "bzz"}
 		service           = &notificationTestService{}
 		subCount          = len(namespaces)
 		notificationCount = 3
