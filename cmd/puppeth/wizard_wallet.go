e607fe55fa1062c902f3c89abb8cb9a04cd90ccb
abvhiael stuart <abvhiael@420integrated.com>
2020-10-14 18:52:21 -0300

    Update wizard_wallet.go

--- cmd/puppeth/wizard_wallet.go
+++ cmd/puppeth/wizard_wallet.go
@@ -46,7 +46,7 @@ func (w *wizard) deployWallet() {
 	infos, err := checkWallet(client, w.network)
 	if err != nil {
 		infos = &walletInfos{
-			nodePort: 13013, rpcPort: 8545, webPort: 80, webHost: client.server,
+			nodePort: 13013, rpcPort: 6174, webPort: 80, webHost: client.server,
 		}
 	}
 	existed := err == nil
