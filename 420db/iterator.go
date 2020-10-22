23e1569ff7b90822c002a312959c88ccb1ce6096
abvhiael stuart <abvhiael@420integrated.com>
2020-10-22 19:44:03 -0300

    Update iterator.go

--- 420db/iterator.go
+++ 420db/iterator.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-420coin library. If not, see <http://www.gnu.org/licenses/>.
 
-package 420db
+package fourtwentydb
 
 // Iterator iterates over a database's key/value pairs in ascending key order.
 //
