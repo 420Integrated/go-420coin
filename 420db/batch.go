fdddda5c2661380e224d414828410edbacd0f7e8
abvhiael stuart <abvhiael@420integrated.com>
2020-10-22 19:35:35 -0300

    Update batch.go

--- 420db/batch.go
+++ 420db/batch.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-420coin library. If not, see <http://www.gnu.org/licenses/>.
 
-package 420db
+package fourtwentydb
 
 // IdealBatchSize defines the size of the data batches should ideally add in one
 // write.
