be132506d28b04e990fdd7c7974d5d1865c8d034
abvhiael stuart <abvhiael@420integrated.com>
2020-10-22 19:40:59 -0300

    Update peer.go

--- 420/peer.go
+++ 420/peer.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-420coin library. If not, see <http://www.gnu.org/licenses/>.
 
-package 420
+package fourtwenty
 
 import (
 	"errors"
