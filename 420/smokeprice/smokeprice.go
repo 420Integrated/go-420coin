2b3e4ae8575600f29b9e5f7e43a370e193fd1137
abvhiael stuart <abvhiael@420integrated.com>
2020-10-22 16:17:23 -0300

    Update smokeprice.go

--- 420/smokeprice/smokeprice.go
+++ 420/smokeprice/smokeprice.go
@@ -31,7 +31,7 @@ import (
 
 const sampleNumber = 3 // Number of transactions sampled in a block
 
-var DefaultMaxPrice = big.NewInt(500 * params.GMarley)
+var DefaultMaxPrice = big.NewInt(500 * params.Maher)
 
 type Config struct {
 	Blocks     int
