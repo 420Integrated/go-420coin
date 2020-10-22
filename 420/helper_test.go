82719e84a2b97d4e1b81015d6f01932b1d273da9
abvhiael stuart <abvhiael@420integrated.com>
2020-10-22 19:40:48 -0300

    Update helper_test.go

--- 420/helper_test.go
+++ 420/helper_test.go
@@ -17,7 +17,7 @@
 // This file contains some shares testing functionality, common to  multiple
 // different files and modules being tested.
 
-package 420
+package fourtwenty
 
 import (
 	"crypto/ecdsa"
