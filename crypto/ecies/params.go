c881fd57fbb62f2755b1addcbfa46247092030f8
abvhiael stuart <abvhiael@420integrated.com>
2020-10-22 20:20:38 -0300

    Update params.go

--- crypto/ecies/params.go
+++ crypto/ecies/params.go
@@ -42,11 +42,11 @@ import (
 	"fmt"
 	"hash"
 
-	420crypto "github.com/420integrated/go-420coin/crypto"
+	fourtwentycrypto "github.com/420integrated/go-420coin/crypto"
 )
 
 var (
-	DefaultCurve                  = 420crypto.S256()
+	DefaultCurve                  = fourtwentycrypto.S256()
 	ErrUnsupportedECDHAlgorithm   = fmt.Errorf("ecies: unsupported ECDH algorithm")
 	ErrUnsupportedECIESParameters = fmt.Errorf("ecies: unsupported ECIES parameters")
 	ErrInvalidKeyLen              = fmt.Errorf("ecies: invalid key size (> %d) in ECIESParams", maxKeyLen)
@@ -106,7 +106,7 @@ var (
 )
 
 var paramsFromCurve = map[elliptic.Curve]*ECIESParams{
-	420crypto.S256(): ECIES_AES128_SHA256,
+	fourtwentycrypto.S256(): ECIES_AES128_SHA256,
 	elliptic.P256():  ECIES_AES128_SHA256,
 	elliptic.P384():  ECIES_AES256_SHA384,
 	elliptic.P521():  ECIES_AES256_SHA512,
