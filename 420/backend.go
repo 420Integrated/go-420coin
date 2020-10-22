e91f380bfdb9b66e387178790bc061876c222861
abvhiael stuart <abvhiael@420integrated.com>
2020-10-22 19:39:07 -0300

    Update backend.go

--- 420/backend.go
+++ 420/backend.go
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-420coin library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package 420 implements the 420coin protocol.
-package 420
+// Package fourtwenty implements the 420coin protocol.
+package fourtwenty
 
 import (
 	"errors"
