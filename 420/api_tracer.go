865bf8d4e4803593cc5d7b65cde5708a6647afb1
abvhiael stuart <abvhiael@420integrated.com>
2020-10-23 19:19:16 -0300

    Update api_tracer.go

--- 420/api_tracer.go
+++ 420/api_tracer.go
@@ -107,19 +107,19 @@ func (api *PrivateDebugAPI) TraceChain(ctx context.Context, start, end rpc.Block
 
 	switch start {
 	case rpc.PendingBlockNumber:
-		from = api.420.miner.PendingBlock()
+		from = api.fourtwenty.miner.PendingBlock()
 	case rpc.LatestBlockNumber:
-		from = api.420.blockchain.CurrentBlock()
+		from = api.fourtwenty.blockchain.CurrentBlock()
 	default:
-		from = api.420.blockchain.GetBlockByNumber(uint64(start))
+		from = api.fourtwenty.blockchain.GetBlockByNumber(uint64(start))
 	}
 	switch end {
 	case rpc.PendingBlockNumber:
-		to = api.420.miner.PendingBlock()
+		to = api.fourtwenty.miner.PendingBlock()
 	case rpc.LatestBlockNumber:
-		to = api.420.blockchain.CurrentBlock()
+		to = api.fourtwenty.blockchain.CurrentBlock()
 	default:
-		to = api.420.blockchain.GetBlockByNumber(uint64(end))
+		to = api.fourtwenty.blockchain.GetBlockByNumber(uint64(end))
 	}
 	// Trace the chain if we've found all our blocks
 	if from == nil {
@@ -147,10 +147,10 @@ func (api *PrivateDebugAPI) traceChain(ctx context.Context, start, end *types.Bl
 
 	// Ensure we have a valid starting state before doing any work
 	origin := start.NumberU64()
-	database := state.NewDatabaseWithCache(api.420.ChainDb(), 16, "") // Chain tracing will probably start at genesis
+	database := state.NewDatabaseWithCache(api.fourtwenty.ChainDb(), 16, "") // Chain tracing will probably start at genesis
 
 	if number := start.NumberU64(); number > 0 {
-		start = api.420.blockchain.GetBlock(start.ParentHash(), start.NumberU64()-1)
+		start = api.fourtwenty.blockchain.GetBlock(start.ParentHash(), start.NumberU64()-1)
 		if start == nil {
 			return nil, fmt.Errorf("parent block #%d not found", number-1)
 		}
@@ -164,7 +164,7 @@ func (api *PrivateDebugAPI) traceChain(ctx context.Context, start, end *types.Bl
 		}
 		// Find the most recent block that has the state available
 		for i := uint64(0); i < reexec; i++ {
-			start = api.420.blockchain.GetBlock(start.ParentHash(), start.NumberU64()-1)
+			start = api.fourtwenty.blockchain.GetBlock(start.ParentHash(), start.NumberU64()-1)
 			if start == nil {
 				break
 			}
@@ -201,12 +201,12 @@ func (api *PrivateDebugAPI) traceChain(ctx context.Context, start, end *types.Bl
 
 			// Fetch and execute the next block trace tasks
 			for task := range tasks {
-				signer := types.MakeSigner(api.420.blockchain.Config(), task.block.Number())
+				signer := types.MakeSigner(api.fourtwenty.blockchain.Config(), task.block.Number())
 
 				// Trace all the transactions contained within
 				for i, tx := range task.block.Transactions() {
 					msg, _ := tx.AsMessage(signer)
-					vmctx := core.NewEVMContext(msg, task.block.Header(), api.420.blockchain, nil)
+					vmctx := core.NewEVMContext(msg, task.block.Header(), api.fourtwenty.blockchain, nil)
 
 					res, err := api.traceTx(ctx, msg, vmctx, task.statedb, config)
 					if err != nil {
@@ -215,7 +215,7 @@ func (api *PrivateDebugAPI) traceChain(ctx context.Context, start, end *types.Bl
 						break
 					}
 					// Only delete empty objects if EIP158/161 (a.k.a Spurious Dragon) is in effect
-					task.statedb.Finalise(api.420.blockchain.Config().IsEIP158(task.block.Number()))
+					task.statedb.Finalise(api.fourtwenty.blockchain.Config().IsEIP158(task.block.Number()))
 					task.results[i] = &txTraceResult{Result: res}
 				}
 				// Stream the result back to the user or abort on teardown
@@ -272,7 +272,7 @@ func (api *PrivateDebugAPI) traceChain(ctx context.Context, start, end *types.Bl
 				logged = time.Now()
 			}
 			// Retrieve the next block to trace
-			block := api.420.blockchain.GetBlockByNumber(number)
+			block := api.fourtwenty.blockchain.GetBlockByNumber(number)
 			if block == nil {
 				failed = fmt.Errorf("block #%d not found", number)
 				break
@@ -289,13 +289,13 @@ func (api *PrivateDebugAPI) traceChain(ctx context.Context, start, end *types.Bl
 				traced += uint64(len(txs))
 			}
 			// Generate the next state snapshot fast without tracing
-			_, _, _, err := api.420.blockchain.Processor().Process(block, statedb, vm.Config{})
+			_, _, _, err := api.fourtwenty.blockchain.Processor().Process(block, statedb, vm.Config{})
 			if err != nil {
 				failed = err
 				break
 			}
 			// Finalize the state so any modifications are written to the trie
-			root, err := statedb.Commit(api.420.blockchain.Config().IsEIP158(block.Number()))
+			root, err := statedb.Commit(api.fourtwenty.blockchain.Config().IsEIP158(block.Number()))
 			if err != nil {
 				failed = err
 				break
@@ -358,11 +358,11 @@ func (api *PrivateDebugAPI) TraceBlockByNumber(ctx context.Context, number rpc.B
 
 	switch number {
 	case rpc.PendingBlockNumber:
-		block = api.420.miner.PendingBlock()
+		block = api.fourtwenty.miner.PendingBlock()
 	case rpc.LatestBlockNumber:
-		block = api.420.blockchain.CurrentBlock()
+		block = api.fourtwenty.blockchain.CurrentBlock()
 	default:
-		block = api.420.blockchain.GetBlockByNumber(uint64(number))
+		block = api.fourtwenty.blockchain.GetBlockByNumber(uint64(number))
 	}
 	// Trace the block if it was found
 	if block == nil {
@@ -374,7 +374,7 @@ func (api *PrivateDebugAPI) TraceBlockByNumber(ctx context.Context, number rpc.B
 // TraceBlockByHash returns the structured logs created during the execution of
 // EVM and returns them as a JSON object.
 func (api *PrivateDebugAPI) TraceBlockByHash(ctx context.Context, hash common.Hash, config *TraceConfig) ([]*txTraceResult, error) {
-	block := api.420.blockchain.GetBlockByHash(hash)
+	block := api.fourtwenty.blockchain.GetBlockByHash(hash)
 	if block == nil {
 		return nil, fmt.Errorf("block %#x not found", hash)
 	}
@@ -405,7 +405,7 @@ func (api *PrivateDebugAPI) TraceBlockFromFile(ctx context.Context, file string,
 // EVM against a block pulled from the pool of bad ones and returns them as a JSON
 // object.
 func (api *PrivateDebugAPI) TraceBadBlock(ctx context.Context, hash common.Hash, config *TraceConfig) ([]*txTraceResult, error) {
-	blocks := api.420.blockchain.BadBlocks()
+	blocks := api.fourtwenty.blockchain.BadBlocks()
 	for _, block := range blocks {
 		if block.Hash() == hash {
 			return api.traceBlock(ctx, block, config)
@@ -418,7 +418,7 @@ func (api *PrivateDebugAPI) TraceBadBlock(ctx context.Context, hash common.Hash,
 // execution of EVM to the local file system and returns a list of files
 // to the caller.
 func (api *PrivateDebugAPI) StandardTraceBlockToFile(ctx context.Context, hash common.Hash, config *StdTraceConfig) ([]string, error) {
-	block := api.420.blockchain.GetBlockByHash(hash)
+	block := api.fourtwenty.blockchain.GetBlockByHash(hash)
 	if block == nil {
 		return nil, fmt.Errorf("block %#x not found", hash)
 	}
@@ -429,7 +429,7 @@ func (api *PrivateDebugAPI) StandardTraceBlockToFile(ctx context.Context, hash c
 // execution of EVM against a block pulled from the pool of bad ones to the
 // local file system and returns a list of files to the caller.
 func (api *PrivateDebugAPI) StandardTraceBadBlockToFile(ctx context.Context, hash common.Hash, config *StdTraceConfig) ([]string, error) {
-	blocks := api.420.blockchain.BadBlocks()
+	blocks := api.fourtwenty.blockchain.BadBlocks()
 	for _, block := range blocks {
 		if block.Hash() == hash {
 			return api.standardTraceBlockToFile(ctx, block, config)
@@ -443,10 +443,10 @@ func (api *PrivateDebugAPI) StandardTraceBadBlockToFile(ctx context.Context, has
 // per transaction, dependent on the requestd tracer.
 func (api *PrivateDebugAPI) traceBlock(ctx context.Context, block *types.Block, config *TraceConfig) ([]*txTraceResult, error) {
 	// Create the parent state database
-	if err := api.420.engine.VerifyHeader(api.420.blockchain, block.Header(), true); err != nil {
+	if err := api.fourtwenty.engine.VerifyHeader(api.420.blockchain, block.Header(), true); err != nil {
 		return nil, err
 	}
-	parent := api.420.blockchain.GetBlock(block.ParentHash(), block.NumberU64()-1)
+	parent := api.fourtwenty.blockchain.GetBlock(block.ParentHash(), block.NumberU64()-1)
 	if parent == nil {
 		return nil, fmt.Errorf("parent %#x not found", block.ParentHash())
 	}
@@ -460,7 +460,7 @@ func (api *PrivateDebugAPI) traceBlock(ctx context.Context, block *types.Block, 
 	}
 	// Execute all the transaction contained within the block concurrently
 	var (
-		signer = types.MakeSigner(api.420.blockchain.Config(), block.Number())
+		signer = types.MakeSigner(api.fourtwenty.blockchain.Config(), block.Number())
 
 		txs     = block.Transactions()
 		results = make([]*txTraceResult, len(txs))
@@ -480,7 +480,7 @@ func (api *PrivateDebugAPI) traceBlock(ctx context.Context, block *types.Block, 
 			// Fetch and execute the next transaction trace tasks
 			for task := range jobs {
 				msg, _ := txs[task.index].AsMessage(signer)
-				vmctx := core.NewEVMContext(msg, block.Header(), api.420.blockchain, nil)
+				vmctx := core.NewEVMContext(msg, block.Header(), api.fourtwenty.blockchain, nil)
 
 				res, err := api.traceTx(ctx, msg, vmctx, task.statedb, config)
 				if err != nil {
@@ -499,9 +499,9 @@ func (api *PrivateDebugAPI) traceBlock(ctx context.Context, block *types.Block, 
 
 		// Generate the next state snapshot fast without tracing
 		msg, _ := tx.AsMessage(signer)
-		vmctx := core.NewEVMContext(msg, block.Header(), api.420.blockchain, nil)
+		vmctx := core.NewEVMContext(msg, block.Header(), api.fourtwenty.blockchain, nil)
 
-		vmenv := vm.NewEVM(vmctx, statedb, api.420.blockchain.Config(), vm.Config{})
+		vmenv := vm.NewEVM(vmctx, statedb, api.fourtwenty.blockchain.Config(), vm.Config{})
 		if _, err := core.ApplyMessage(vmenv, msg, new(core.SmokePool).AddSmoke(msg.Smoke())); err != nil {
 			failed = err
 			break
@@ -531,10 +531,10 @@ func (api *PrivateDebugAPI) standardTraceBlockToFile(ctx context.Context, block 
 		}
 	}
 	// Create the parent state database
-	if err := api.420.engine.VerifyHeader(api.420.blockchain, block.Header(), true); err != nil {
+	if err := api.fourtwenty.engine.VerifyHeader(api.fourtwenty.blockchain, block.Header(), true); err != nil {
 		return nil, err
 	}
-	parent := api.420.blockchain.GetBlock(block.ParentHash(), block.NumberU64()-1)
+	parent := api.fourtwenty.blockchain.GetBlock(block.ParentHash(), block.NumberU64()-1)
 	if parent == nil {
 		return nil, fmt.Errorf("parent %#x not found", block.ParentHash())
 	}
@@ -561,14 +561,14 @@ func (api *PrivateDebugAPI) standardTraceBlockToFile(ctx context.Context, block 
 
 	// Execute transaction, either tracing all or just the requested one
 	var (
-		signer = types.MakeSigner(api.420.blockchain.Config(), block.Number())
+		signer = types.MakeSigner(api.fourtwenty.blockchain.Config(), block.Number())
 		dumps  []string
 	)
 	for i, tx := range block.Transactions() {
 		// Prepare the trasaction for un-traced execution
 		var (
 			msg, _ = tx.AsMessage(signer)
-			vmctx  = core.NewEVMContext(msg, block.Header(), api.420.blockchain, nil)
+			vmctx  = core.NewEVMContext(msg, block.Header(), api.fourtwenty.blockchain, nil)
 
 			vmConf vm.Config
 			dump   *os.File
@@ -595,7 +595,7 @@ func (api *PrivateDebugAPI) standardTraceBlockToFile(ctx context.Context, block 
 			}
 		}
 		// Execute the transaction and flush any traces to disk
-		vmenv := vm.NewEVM(vmctx, statedb, api.420.blockchain.Config(), vmConf)
+		vmenv := vm.NewEVM(vmctx, statedb, api.fourtwenty.blockchain.Config(), vmConf)
 		_, err = core.ApplyMessage(vmenv, msg, new(core.SmokePool).AddSmoke(msg.Smoke()))
 		if writer != nil {
 			writer.Flush()
@@ -635,16 +635,16 @@ func containsTx(block *types.Block, hash common.Hash) bool {
 // attempted to be reexecuted to generate the desired state.
 func (api *PrivateDebugAPI) computeStateDB(block *types.Block, reexec uint64) (*state.StateDB, error) {
 	// If we have the state fully available, use that
-	statedb, err := api.420.blockchain.StateAt(block.Root())
+	statedb, err := api.fourtwenty.blockchain.StateAt(block.Root())
 	if err == nil {
 		return statedb, nil
 	}
 	// Otherwise try to reexec blocks until we find a state or reach our limit
 	origin := block.NumberU64()
-	database := state.NewDatabaseWithCache(api.420.ChainDb(), 16, "")
+	database := state.NewDatabaseWithCache(api.fourtwenty.ChainDb(), 16, "")
 
 	for i := uint64(0); i < reexec; i++ {
-		block = api.420.blockchain.GetBlock(block.ParentHash(), block.NumberU64()-1)
+		block = api.fourtwenty.blockchain.GetBlock(block.ParentHash(), block.NumberU64()-1)
 		if block == nil {
 			break
 		}
@@ -673,15 +673,15 @@ func (api *PrivateDebugAPI) computeStateDB(block *types.Block, reexec uint64) (*
 			logged = time.Now()
 		}
 		// Retrieve the next block to regenerate and process it
-		if block = api.420.blockchain.GetBlockByNumber(block.NumberU64() + 1); block == nil {
+		if block = api.fourtwenty.blockchain.GetBlockByNumber(block.NumberU64() + 1); block == nil {
 			return nil, fmt.Errorf("block #%d not found", block.NumberU64()+1)
 		}
-		_, _, _, err := api.420.blockchain.Processor().Process(block, statedb, vm.Config{})
+		_, _, _, err := api.fourtwenty.blockchain.Processor().Process(block, statedb, vm.Config{})
 		if err != nil {
 			return nil, fmt.Errorf("processing block %d failed: %v", block.NumberU64(), err)
 		}
 		// Finalize the state so any modifications are written to the trie
-		root, err := statedb.Commit(api.420.blockchain.Config().IsEIP158(block.Number()))
+		root, err := statedb.Commit(api.fourtwenty.blockchain.Config().IsEIP158(block.Number()))
 		if err != nil {
 			return nil, err
 		}
@@ -703,7 +703,7 @@ func (api *PrivateDebugAPI) computeStateDB(block *types.Block, reexec uint64) (*
 // and returns them as a JSON object.
 func (api *PrivateDebugAPI) TraceTransaction(ctx context.Context, hash common.Hash, config *TraceConfig) (interface{}, error) {
 	// Retrieve the transaction and assemble its EVM context
-	tx, blockHash, _, index := rawdb.ReadTransaction(api.420.ChainDb(), hash)
+	tx, blockHash, _, index := rawdb.ReadTransaction(api.fourtwenty.ChainDb(), hash)
 	if tx == nil {
 		return nil, fmt.Errorf("transaction %#x not found", hash)
 	}
@@ -712,7 +712,7 @@ func (api *PrivateDebugAPI) TraceTransaction(ctx context.Context, hash common.Ha
 		reexec = *config.Reexec
 	}
 	// Retrieve the block
-	block := api.420.blockchain.GetBlockByHash(blockHash)
+	block := api.fourtwenty.blockchain.GetBlockByHash(blockHash)
 	if block == nil {
 		return nil, fmt.Errorf("block %#x not found", blockHash)
 	}
@@ -727,16 +727,16 @@ func (api *PrivateDebugAPI) TraceTransaction(ctx context.Context, hash common.Ha
 // TraceCall lets you trace a given 420_call. It collects the structured logs created during the execution of EVM
 // if the given transaction was added on top of the provided block and returns them as a JSON object.
 // You can provide -2 as a block number to trace on top of the pending block.
-func (api *PrivateDebugAPI) TraceCall(ctx context.Context, args 420api.CallArgs, blockNrOrHash rpc.BlockNumberOrHash, config *TraceConfig) (interface{}, error) {
+func (api *PrivateDebugAPI) TraceCall(ctx context.Context, args fourtwentyapi.CallArgs, blockNrOrHash rpc.BlockNumberOrHash, config *TraceConfig) (interface{}, error) {
 	// First try to retrieve the state
-	statedb, header, err := api.420.APIBackend.StateAndHeaderByNumberOrHash(ctx, blockNrOrHash)
+	statedb, header, err := api.fourtwenty.APIBackend.StateAndHeaderByNumberOrHash(ctx, blockNrOrHash)
 	if err != nil {
 		// Try to retrieve the specified block
 		var block *types.Block
 		if hash, ok := blockNrOrHash.Hash(); ok {
-			block = api.420.blockchain.GetBlockByHash(hash)
+			block = api.fourtwenty.blockchain.GetBlockByHash(hash)
 		} else if number, ok := blockNrOrHash.Number(); ok {
-			block = api.420.blockchain.GetBlockByNumber(uint64(number))
+			block = api.fourtwenty.blockchain.GetBlockByNumber(uint64(number))
 		}
 		if block == nil {
 			return nil, fmt.Errorf("block %v not found: %v", blockNrOrHash, err)
@@ -753,8 +753,8 @@ func (api *PrivateDebugAPI) TraceCall(ctx context.Context, args 420api.CallArgs,
 	}
 
 	// Execute the trace
-	msg := args.ToMessage(api.420.APIBackend.RPCSmokeCap())
-	vmctx := core.NewEVMContext(msg, header, api.420.blockchain, nil)
+	msg := args.ToMessage(api.fourtwenty.APIBackend.RPCSmokeCap())
+	vmctx := core.NewEVMContext(msg, header, api.fourtwenty.blockchain, nil)
 	return api.traceTx(ctx, msg, vmctx, statedb, config)
 }
 
@@ -795,7 +795,7 @@ func (api *PrivateDebugAPI) traceTx(ctx context.Context, message core.Message, v
 		tracer = vm.NewStructLogger(config.LogConfig)
 	}
 	// Run the transaction with tracing enabled.
-	vmenv := vm.NewEVM(vmctx, statedb, api.420.blockchain.Config(), vm.Config{Debug: true, Tracer: tracer})
+	vmenv := vm.NewEVM(vmctx, statedb, api.fourtwenty.blockchain.Config(), vm.Config{Debug: true, Tracer: tracer})
 
 	result, err := core.ApplyMessage(vmenv, message, new(core.SmokePool).AddSmoke(message.Smoke()))
 	if err != nil {
@@ -809,11 +809,11 @@ func (api *PrivateDebugAPI) traceTx(ctx context.Context, message core.Message, v
 		if len(result.Revert()) > 0 {
 			returnVal = fmt.Sprintf("%x", result.Revert())
 		}
-		return &420api.ExecutionResult{
+		return &fourtwentyapi.ExecutionResult{
 			Smoke:         result.UsedSmoke,
 			Failed:      result.Failed(),
 			ReturnValue: returnVal,
-			StructLogs:  420api.FormatLogs(tracer.StructLogs()),
+			StructLogs:  fourtwentyapi.FormatLogs(tracer.StructLogs()),
 		}, nil
 
 	case *tracers.Tracer:
@@ -827,7 +827,7 @@ func (api *PrivateDebugAPI) traceTx(ctx context.Context, message core.Message, v
 // computeTxEnv returns the execution environment of a certain transaction.
 func (api *PrivateDebugAPI) computeTxEnv(block *types.Block, txIndex int, reexec uint64) (core.Message, vm.Context, *state.StateDB, error) {
 	// Create the parent state database
-	parent := api.420.blockchain.GetBlock(block.ParentHash(), block.NumberU64()-1)
+	parent := api.fourtwenty.blockchain.GetBlock(block.ParentHash(), block.NumberU64()-1)
 	if parent == nil {
 		return nil, vm.Context{}, nil, fmt.Errorf("parent %#x not found", block.ParentHash())
 	}
@@ -841,17 +841,17 @@ func (api *PrivateDebugAPI) computeTxEnv(block *types.Block, txIndex int, reexec
 	}
 
 	// Recompute transactions up to the target index.
-	signer := types.MakeSigner(api.420.blockchain.Config(), block.Number())
+	signer := types.MakeSigner(api.fourtwenty.blockchain.Config(), block.Number())
 
 	for idx, tx := range block.Transactions() {
 		// Assemble the transaction call message and return if the requested offset
 		msg, _ := tx.AsMessage(signer)
-		context := core.NewEVMContext(msg, block.Header(), api.420.blockchain, nil)
+		context := core.NewEVMContext(msg, block.Header(), api.fourtwenty.blockchain, nil)
 		if idx == txIndex {
 			return msg, context, statedb, nil
 		}
 		// Not yet the searched for transaction, execute on top of the current state
-		vmenv := vm.NewEVM(context, statedb, api.420.blockchain.Config(), vm.Config{})
+		vmenv := vm.NewEVM(context, statedb, api.fourtwenty.blockchain.Config(), vm.Config{})
 		if _, err := core.ApplyMessage(vmenv, msg, new(core.SmokePool).AddSmoke(tx.Smoke())); err != nil {
 			return nil, vm.Context{}, nil, fmt.Errorf("transaction %#x failed: %v", tx.Hash(), err)
 		}
