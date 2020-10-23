0ca0e0ddf5036715c16a378cec2da4070edec523
abvhiael stuart <abvhiael@420integrated.com>
2020-10-23 01:05:52 -0300

    Update costtracker.go

--- les/costtracker.go
+++ les/costtracker.go
@@ -115,7 +115,7 @@ const (
 // changes in the cost factor can be applied immediately without always notifying
 // the clients about the changed cost tables.
 type costTracker struct {
-	db     420db.Database
+	db     fourtwentydb.Database
 	stopCh chan chan struct{}
 
 	inSizeFactor  float64
@@ -137,7 +137,7 @@ type costTracker struct {
 
 // newCostTracker creates a cost tracker and loads the cost factor statistics from the database.
 // It also returns the minimum capacity that can be assigned to any peer.
-func newCostTracker(db 420db.Database, config *420.Config) (*costTracker, uint64) {
+func newCostTracker(db fourtwentydb.Database, config *420.Config) (*costTracker, uint64) {
 	utilTarget := float64(config.LightServ) * flowcontrol.FixedPointMultiplier / 100
 	ct := &costTracker{
 		db:         db,
