444bde105a26d9514ebb937968a2948cb661e4d4
abvhiael stuart <abvhiael@420integrated.com>
2020-10-24 14:57:43 -0300

    Update websocket_test.go

--- rpc/websocket_test.go
+++ rpc/websocket_test.go
@@ -130,7 +130,7 @@ func TestClientWebsocketPing(t *testing.T) {
 		t.Fatalf("client dial error: %v", err)
 	}
 	resultChan := make(chan int)
-	sub, err := client.fourtwentySubscribe(ctx, resultChan, "foo")
+	sub, err := client.FourtwentySubscribe(ctx, resultChan, "foo")
 	if err != nil {
 		t.Fatalf("client subscribe error: %v", err)
 	}
