b0642d1ec3e8c5d232a30af4f524c529e8d69de8
abvhiael stuart <abvhiael@420integrated.com>
2020-10-23 21:38:40 -0300

    Update service.go

--- graphql/service.go
+++ graphql/service.go
@@ -24,7 +24,7 @@ import (
 )
 
 // New constructs a new GraphQL service instance.
-func New(stack *node.Node, backend 420api.Backend, cors, vhosts []string) error {
+func New(stack *node.Node, backend fourtwentyapi.Backend, cors, vhosts []string) error {
 	if backend == nil {
 		panic("missing backend")
 	}
@@ -34,7 +34,7 @@ func New(stack *node.Node, backend 420api.Backend, cors, vhosts []string) error 
 
 // newHandler returns a new `http.Handler` that will answer GraphQL queries.
 // It additionally exports an interactive query browser on the / endpoint.
-func newHandler(stack *node.Node, backend 420api.Backend, cors, vhosts []string) error {
+func newHandler(stack *node.Node, backend fourtwentyapi.Backend, cors, vhosts []string) error {
 	q := Resolver{backend}
 
 	s, err := graphql.ParseSchema(schema, &q)
