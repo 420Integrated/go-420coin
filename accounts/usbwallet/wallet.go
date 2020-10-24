f640cd68946e0a35ed51b0a91497c393040e1ef0
abvhiael stuart <abvhiael@420integrated.com>
2020-10-23 22:32:52 -0300

    Update wallet.go

--- accounts/usbwallet/wallet.go
+++ accounts/usbwallet/wallet.go
@@ -85,7 +85,7 @@ type wallet struct {
 
 	deriveNextPaths []accounts.DerivationPath // Next derivation paths for account auto-discovery (multiple bases supported)
 	deriveNextAddrs []common.Address          // Next derived account addresses for auto-discovery (multiple bases supported)
-	deriveChain     420coin.ChainStateReader // Blockchain state reader to discover used account with
+	deriveChain     fourtwentycoin.ChainStateReader // Blockchain state reader to discover used account with
 	deriveReq       chan chan struct{}        // Channel to request a self-derivation on
 	deriveQuit      chan chan error           // Channel to terminate the self-deriver with
 
@@ -503,7 +503,7 @@ func (w *wallet) Derive(path accounts.DerivationPath, pin bool) (accounts.Accoun
 //
 // You can disable automatic account discovery by calling SelfDerive with a nil
 // chain state reader.
-func (w *wallet) SelfDerive(bases []accounts.DerivationPath, chain 420coin.ChainStateReader) {
+func (w *wallet) SelfDerive(bases []accounts.DerivationPath, chain fourtwentycoin.ChainStateReader) {
 	w.stateLock.Lock()
 	defer w.stateLock.Unlock()
 
