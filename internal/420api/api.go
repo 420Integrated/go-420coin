037a4681510ca186395d6a1155c847206d42bfc5
abvhiael stuart <abvhiael@420integrated.com>
2020-11-11 18:13:50 -0400

    Update api.go

--- internal/420api/api.go
+++ internal/420api/api.go
@@ -963,6 +963,9 @@ func DoEstimateSmoke(ctx context.Context, b Backend, args CallArgs, blockNrOrHas
 		if err != nil {
 			return 0, err
 		}
+		if block == nil {
+			return 0, errors.New("block not found")
+		}
 		hi = block.SmokeLimit()
 	}
 	// Recap the highest smoke limit with account's available balance.
