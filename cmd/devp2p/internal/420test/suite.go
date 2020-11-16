7cc530dc65cb429244e45c2b336e717573fec8e4
abvhiael stuart <abvhiael@420integrated.com>
2020-11-16 14:59:21 -0400

    Update suite.go

--- cmd/devp2p/internal/420test/suite.go
+++ cmd/devp2p/internal/420test/suite.go
@@ -45,7 +45,7 @@ type Suite struct {
 	fullChain *Chain
 }
 
-// NewSuite creates and returns a new eth-test suite that can
+// NewSuite creates and returns a new fourtwenty-test suite that can
 // be used to test the given node against the given blockchain
 // data.
 func NewSuite(dest *enode.Node, chainfile string, genesisfile string) *Suite {
