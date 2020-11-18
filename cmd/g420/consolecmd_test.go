1fee814770af26d99552d3519e63fd0899ed0f0b
abvhiael stuart <abvhiael@420integrated.com>
2020-11-18 14:37:55 -0400

    Update consolecmd_test.go

--- cmd/g420/consolecmd_test.go
+++ cmd/g420/consolecmd_test.go
@@ -43,7 +43,7 @@ func TestConsoleWelcome(t *testing.T) {
 	// Start a g420 console, make sure it's cleaned up and terminate the console
 	g420 := runG420(t,
 		"--port", "0", "--maxpeers", "0", "--nodiscover", "--nat", "none",
-		"--fourtwentycoinbase", coinbase,
+		"--Fourtwentycoinbase", coinbase,
 		"console")
 
 	// Gather all the infos the welcome message needs to contain
@@ -61,7 +61,7 @@ func TestConsoleWelcome(t *testing.T) {
 Welcome to the G420 JavaScript console!
 
 instance: G420/v{{g420ver}}/{{goos}}-{{goarch}}/{{gover}}
-coinbase: {{.fourtwentycoinbase}}
+coinbase: {{.Fourtwentycoinbase}}
 at block: 0 ({{niltime}})
  datadir: {{.Datadir}}
  modules: {{apis}}
@@ -86,7 +86,7 @@ func TestIPCAttachWelcome(t *testing.T) {
 	}
 	g420 := runG420(t,
 		"--port", "0", "--maxpeers", "0", "--nodiscover", "--nat", "none",
-		"--fourtwentycoinbase", coinbase, "--ipcpath", ipc)
+		"--Fourtwentycoinbase", coinbase, "--ipcpath", ipc)
 
 	defer func() {
 		g420.Interrupt()
@@ -103,7 +103,7 @@ func TestHTTPAttachWelcome(t *testing.T) {
 	port := strconv.Itoa(trulyRandInt(1024, 65536)) // Yeah, sometimes this will fail, sorry :P
 	g420 := runG420(t,
 		"--port", "0", "--maxpeers", "0", "--nodiscover", "--nat", "none",
-		"--fourtwentycoinbase", coinbase, "--http", "--http.port", port)
+		"--Fourtwentycoinbase", coinbase, "--http", "--http.port", port)
 	defer func() {
 		g420.Interrupt()
 		g420.ExpectExit()
@@ -120,7 +120,7 @@ func TestWSAttachWelcome(t *testing.T) {
 
 	g420 := runG420(t,
 		"--port", "0", "--maxpeers", "0", "--nodiscover", "--nat", "none",
-		"--fourtwentycoinbase", coinbase, "--ws", "--ws.port", port)
+		"--Fourtwentycoinbase", coinbase, "--ws", "--ws.port", port)
 	defer func() {
 		g420.Interrupt()
 		g420.ExpectExit()
@@ -142,7 +142,7 @@ func testAttachWelcome(t *testing.T, g420 *testg420, endpoint, apis string) {
 	attach.SetTemplateFunc("goarch", func() string { return runtime.GOARCH })
 	attach.SetTemplateFunc("gover", runtime.Version)
 	attach.SetTemplateFunc("g420ver", func() string { return params.VersionWithCommit("", "") })
-	attach.SetTemplateFunc("fourtwentycoinbase", func() string { return g420.fourtwentycoinbase })
+	attach.SetTemplateFunc("Fourtwentycoinbase", func() string { return g420.Fourtwentycoinbase })
 	attach.SetTemplateFunc("niltime", func() string {
 		return time.Unix(0, 0).Format("Mon Jan 02 2006 15:04:05 GMT-0700 (MST)")
 	})
@@ -155,7 +155,7 @@ func testAttachWelcome(t *testing.T, g420 *testg420, endpoint, apis string) {
 Welcome to the G420 JavaScript console!
 
 instance: G420/v{{g420ver}}/{{goos}}-{{goarch}}/{{gover}}
-coinbase: {{fourtwentycoinbase}}
+coinbase: {{Fourtwentycoinbase}}
 at block: 0 ({{niltime}}){{if ipc}}
  datadir: {{datadir}}{{end}}
  modules: {{apis}}
