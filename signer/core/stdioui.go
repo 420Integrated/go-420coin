6d7056746e9abdd55f0ec443c0d2126fea3188ff
abvhiael stuart <abvhiael@420integrated.com>
2020-11-17 20:28:20 -0400

    Update stdioui.go

--- signer/core/stdioui.go
+++ signer/core/stdioui.go
@@ -97,7 +97,7 @@ func (ui *StdIOUI) ShowInfo(message string) {
 		log.Info("Error calling 'ui_showInfo'", "exc", err.Error(), "msg", message)
 	}
 }
-func (ui *StdIOUI) OnApprovedTx(tx 420api.SignTransactionResult) {
+func (ui *StdIOUI) OnApprovedTx(tx fourtwentyapi.SignTransactionResult) {
 	err := ui.notify("ui_onApprovedTx", tx)
 	if err != nil {
 		log.Info("Error calling 'ui_onApprovedTx'", "exc", err.Error(), "tx", tx)
