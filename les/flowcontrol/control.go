73cbb2fe23120ceb2203617831941c5138e5d1fd
abvhiael stuart <abvhiael@420integrated.com>
2020-12-02 15:51:39 -0400

    Update control.go

--- les/flowcontrol/control.go
+++ les/flowcontrol/control.go
@@ -19,6 +19,7 @@ package flowcontrol
 
 import (
 	"fmt"
+	"math"
 	"sync"
 	"time"
 
@@ -315,7 +316,10 @@ const safetyMargin = time.Millisecond
 func (node *ServerNode) CanSend(maxCost uint64) (time.Duration, float64) {
 	node.lock.RLock()
 	defer node.lock.RUnlock()
-
+	
+	if node.params.BufLimit == 0 {
+		return time.Duration(math.MaxInt64), 0
+	}
 	now := node.clock.Now()
 	node.recalcBLE(now)
 	maxCost += uint64(safetyMargin) * node.params.MinRecharge / uint64(fcTimeConst)
